46f2634135197c7e69d0c48e4371b2962df7b0bf
polina <pooina123@gmail.com>
2023-08-10 14:33:51 +0500

    Добавлен .gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int spent = 20;
+        int mile = 1;
+        int ticket_price = 96_840;
+        int bonus = ticket_price / spent;
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
